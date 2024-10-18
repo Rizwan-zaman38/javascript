6413569d15168c1119ca5260a37dfa1181f45923
Rizwan-zaman38 <rizwanzaman38@gmail.com>
2024-10-18 05:43:03 -0700

    18 oct

--- index.js
+++ index.js
@@ -53,39 +53,277 @@
 
 
 
-// let person{
-//     name = "Ahamad",
-//     age = 20,
-//     gender ="male",
-//     education = "bs",
-//     address = "lahore",
-//     hobby = "coding"
+// let person = {
+
+//     let name = parseInt(prompt("Ahamad"))
+//     let age = prompt(20)
+//     let education = parseInt(prompt("bs"))
+//     let address = parseInt(prompt("lahore"))
+//     let phone = prompt(03243434345)
+//     let lastDegree = parseInt(prompt("Fsc"))
+
 // }
 
-// console.log(`The Name is ${person.name} and Age is ${person.age} and the Education ${person.education}`)
+// console.log(`The Name is ${name} and Age is ${age} and the Education ${education} and the Address of the ${name} is ${address}.The phone number is ${phone} and last degree of ${name} is ${lastDegree}`)
 
 
-let email = "rizwanzaman38@gmail.com";
-let pass = 12345;
-let total_bal = 10000;
+// let person1 = Ali
+// let person2 = Ahmad
 
-let email1 = prompt("Enter your Email")
-if(email!=email1){
-    alert("You enterd the incorrect Email.")
-    let email1 = prompt("Enter your correct Email")
+// person1&&person2 ? alert("Today class is held"):alert("Class not held")
+
+
+
+
+
+
+// let email = "rizwanzaman38@gmail.com";
+// let pass = 12345;
+// let total_bal = 100000;
+
+// let email1 = prompt("Enter your Email")
+
+// while (email != email1) {
+//     alert("You enterd the incorrect Email.")
+//     let email1 = prompt("Enter your correct Email")
+
+//     if (email == email1) {
+//         let pass1 = parseInt(prompt("Enter the password"))
+
+//         while (pass != pass1) {
+//             alert("You enterd the incorrect Password.")
+//             let pass1 = parseInt(prompt("Enter the correct password"))
+
+//             if (email == email1 && pass == pass1) {
+//                 alert("You are Sucessfully login to Your Account.\n What you want? (type ` 1 ` for Deposit or ` 2 ` for Withdraw or ` 3 ` for Balance Inquiry)")
+
+//                 let num = prompt("Enter the number of your choice: ")
+
+//                 if (num == 1) {
+//                     let amount = prompt("Enter the amount you want to Deposit")
+//                     total_bal += parseInt(amount)
+//                     alert("Your current balance after deposit is: " + total_bal)
+//                 }
+//                 else if (num == 2) {
+//                     let amount = prompt("Enter the amount you want to Withdraw")
+//                     total_bal -= parseInt(amount)
+//                     alert("Your remaining balance after Withdraw is: " + total_bal)
+//                 }
+//                 else if (num == 3) {
+//                     alert("Your Current Balance is: " + total_bal)
+//                 }
+//                 else {
+//                     alert("You enter the invalid choice.")
+//                 }
+//                 alert("Thank You for Visiting")
+//                 break;
+//             }
+//             break;
+
+//         }
+//         if (pass == pass1) {
+//             alert("You are Sucessfully login to Your Account.\n What you want? (type ` 1 ` for Deposit or ` 2 ` for Withdraw or ` 3 ` for Balance Inquiry)")
+
+//             let num = prompt("Enter the number of your choice: ")
+
+//             if (num == 1) {
+//                 let amount = prompt("Enter the amount you want to Deposit")
+//                 total_bal += parseInt(amount)
+//                 alert("Your current balance after deposit is: " + total_bal)
+//             }
+//             else if (num == 2) {
+//                 let amount = prompt("Enter the amount you want to Withdraw")
+//                 total_bal -= parseInt(amount)
+//                 alert("Your remaining balance after Withdraw is: " + total_bal)
+//             }
+//             else if (num == 3) {
+//                 alert("Your Current Balance is: " + total_bal)
+//             }
+//             else {
+//                 alert("You enter the invalid choice.")
+//             }
+//             alert("Thank You for Visiting")
+//         }
+//         break;
+//     }
+//     break;
+// }
+
+// if (email == email1) {
+//     let pass1 = parseInt(prompt("Enter the password"))
+
+//     if (email == email1 && pass == pass1) {
+//         alert("You are Sucessfully login to Your Account.\n What you want? (type ` 1 ` for Deposit or ` 2 ` for Withdraw or ` 3 ` for Balance Inquiry)")
+
+//         let num = prompt("Enter the number of your choice: ")
+
+//         if (num == 1) {
+//             let amount = prompt("Enter the amount you want to Deposit")
+//             total_bal += parseInt(amount)
+//             alert("Your current balance after deposit is: " + total_bal)
+//         }
+//         else if (num == 2) {
+//             let amount = prompt("Enter the amount you want to Withdraw")
+//             total_bal -= parseInt(amount)
+//             alert("Your remaining balance after Withdraw is: " + total_bal)
+//         }
+//         else if (num == 3) {
+//             alert("Your Current Balance is: " + total_bal)
+//         }
+//         else {
+//             alert("You enter the invalid choice.")
+//         }
+//         alert("Thank You for Visiting")
+//     }
+// }
+
+
+
+// let pass1 = parseInt(prompt("Enter the password"))
+// if(pass!=pass1){
+//     alert("You enterd the incorrect Password.")
+//     // let pass1 = parseInt(prompt("Enter the correct password"))
+// }
+
+// if(email!=email1 && pass!=pass1){
+//     alert("Plz Enter the Right Credientials.\n Thank You.")
+// }
+
+// if(email==email1 && pass==pass1){
+//     alert("You are Sucessfully login to Your Account.\n What you want? (type ` 1 ` for Deposit or ` 2 ` for Withdraw or ` 3 ` for Balance Inquiry)")
+
+//     let num = prompt("Enter the number of your choice: ")
+
+//     if(num==1){
+//         let amount = prompt("Enter the amount you want to Deposit")
+//         total_bal += parseInt(amount)
+//         alert("Your current balance after deposit is: " + total_bal)
+//     }
+//     else if(num==2){
+//         let amount = prompt("Enter the amount you want to Withdraw")
+//         total_bal -= parseInt(amount)
+//         alert("Your remaining balance after Withdraw is: " + total_bal)
+//     }
+//     else if(num==3){
+//         alert("Your Current Balance is: " + total_bal)
+//     }
+//     else{
+//         alert("You enter the invalid choice.")
+//     }
+
+// }
+
+
+
+// let personAccount = true;
+
+// if(personAccount = true){
+//     console.log("Account is active and your balance is &1000")
+// }
+// else{
+//     console.log("Account is inactive please open account first")
+// }
+
+// let accountNumber = 1234567
+// let emailid = rizwan38
+
+// let personAccount = parseInt(prompt("Enter your Account number: "))
+
+// let email = prompt("Enter your email: ")
+
+// if(accountNumber==personAccount){
+//     alert("Account is active and your balance is &1000")
+// }
+// else{
+//     alert("please enter the valid credentials")
+// }
+
+// let age = 18;
+
+// age >= 18 ? alert("You are eligible"):alert("You are not eligible.")
+
+
+// let purchase = parseInt(prompt("Enter your purchase amount."))
+
+// let totalAmount = purchase ;
+
+// if(purchase>=2000 && purchase<2500){
+//     totalAmount = purchase - 200;
+//     alert(`Your total bill after discount is ${totalAmount}`)
+// }
+// else if(purchase>=1500 && purchase<2000){
+//     totalAmount = purchase - 150;
+//     alert(`Your total bill after discount is ${totalAmount}`)
+// }
+// else if(purchase>=1000 && purchase<1500){
+//     totalAmount = purchase - 100;
+//     alert(`Your total bill after discount is ${totalAmount}`)
+// }
+// else if(purchase<1000 && purchase>100){
+//     totalAmount = purchase - 50;
+//     alert(`Your total bill after discount is ${totalAmount}`)
+// }
+// else{
+//     alert("no discount")
+// }
+
+
+let products = { 
+    product: {
+    "category": "beauty",
+    "price": 9.99,
+    "discountPercentage": 7.17,
+    "rating": 4.94,
+    "stock": 5,
+    "tags": [
+    "beauty",
+    "mascara"
+    ],
+    "brand": "Essence",
+    "sku": "RCH45Q1A",
+    "weight": 2,
+    "dimensions": {
+    "width": 23.17,
+    "height": 14.43,
+    "depth": 28.01
+    },
+    "warrantyInformation": "1 month warranty",
+    "shippingInformation": "Ships in 1 month",
+    "availabilityStatus": "Low Stock",
+    "reviews": [
+    {
+    "rating": 2,
+    "comment": "Very unhappy with my purchase!",
+    "date": "2024-05-23T08:56:21.618Z",
+    "reviewerName": "John Doe",
+    "reviewerEmail": "john.doe@x.dummyjson.com"
+    },
+    {
+    "rating": 2,
+    "comment": "Not as described!",
+    "date": "2024-05-23T08:56:21.618Z",
+    "reviewerName": "Nolan Gonzalez",
+    "reviewerEmail": "nolan.gonzalez@x.dummyjson.com"
+    },
+    {
+    "rating": 5,
+    "comment": "Very satisfied!",
+    "date": "2024-05-23T08:56:21.618Z",
+    "reviewerName": "Scarlett Wright",
+    "reviewerEmail": "scarlett.wright@x.dummyjson.com"
+    }
+    ],
+    "returnPolicy": "30 days return policy",
+    "minimumOrderQuantity": 24,
+    "meta": {
+    "createdAt": "2024-05-23T08:56:21.618Z",
+    "updatedAt": "2024-05-23T08:56:21.618Z",
+    "barcode": "9164035109868",
+    "qrCode": "https://assets.dummyjson.com/public/qr-code.png"
+    },
+}
 }
 
-
-let pass1 = parseInt(prompt("Enter the password"))
-if(pass!=pass1){
-    alert("You enterd the incorrect Password.")
-    let pass1 = parseInt(prompt("Enter the correct password"))
-}
-
-
-if(email==email1 && pass==pass1){
-    alert("You are Sucessfully login to Your Account.\n Your total balance is \n What you want?")
-}
-if(email!=email1 && pass!=pass1){
-    alert("Plz Enter the Right Credientials.\n Thank You.")
-}
+console.log(products.product.tags[0])
+console.log(products.product.dimensions)
+console.log(products.product.reviews[0].comment)
